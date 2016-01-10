6584162e2d267517ae43bf80778c8a3c988695ea
aminami1127 <aminami1127@users.noreply.github.com>
2016-01-10 17:34:05 +0900

    garden

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 import $ from 'jquery';
+import me from 'magiceye'
+import dm from 'TextDepthMapper'
 
-console.log("jQuery version: " + $.fn.jquery);
+function renderMagicEye() {
+  me.render({
+    el: 'magic-eye',
+    colors: generatePalette(10),
+    depthMapper: new me.TextDepthMapper("OK!")
+  });
+}
+
+function randomRGBa() {
+  return [Math.floor(Math.random() * 256),
+          Math.floor(Math.random() * 256),
+          Math.floor(Math.random() * 256),
+          255];
+}
+
+function generatePalette(numColors) {
+  var palette = [];
+  for (var i = 0; i < numColors; i++) {
+    palette.push(randomRGBa());
+  }
+  return palette;
+}
+
+$(function(){
+  renderMagicEye();
+});
