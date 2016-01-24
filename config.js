9ad97348629b8ea8f7f56941a140a4ba405ede84
aminami1127 <aminami1127@users.noreply.github.com>
2016-01-24 21:54:26 +0900

    add bootstrap and lodash

--- config.js
+++ config.js
@@ -18,8 +18,10 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "backbone": "npm:backbone@1.2.3",
     "backbone.marionette": "npm:backbone.marionette@2.4.4",
+    "bootstrap": "github:twbs/bootstrap@3.3.6",
     "core-js": "npm:core-js@1.2.6",
     "jquery": "npm:jquery@2.2.0",
+    "lodash": "npm:lodash@4.0.0",
     "magiceye": "github:peeinears/MagicEye.js@master",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
@@ -32,6 +34,9 @@ System.config({
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
+    },
+    "github:twbs/bootstrap@3.3.6": {
+      "jquery": "github:components/jquery@2.2.0"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
@@ -68,6 +73,9 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:lodash@4.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
