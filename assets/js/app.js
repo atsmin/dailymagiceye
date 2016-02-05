8b661b44edc6da5715c0bd536b9b9864d9007a0d
aminami1127 <aminami1127@users.noreply.github.com>
2016-02-06 08:22:21 +0900

    add semicorons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
-import Marionette from 'backbone.marionette'
+import Marionette from 'backbone.marionette';
 
-import * as controller from 'assets/js/controller'
-import textmapper from 'assets/js/TextDepthMapper'
+import * as controller from 'assets/js/controller';
+import textmapper from 'assets/js/TextDepthMapper';
 
 $(function(){
   var App = new Marionette.Application();
