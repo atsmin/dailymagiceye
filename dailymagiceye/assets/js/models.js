ba426d3be1fda52e33cdde25b619d4ce613e4ef0
aminami1127 <aminami1127@users.noreply.github.com>
2016-03-03 20:59:00 +0900

    change to use ES6 template string

--- dailymagiceye/assets/js/models.js
+++ dailymagiceye/assets/js/models.js
@@ -48,35 +48,25 @@ export var TextList = Backbone.Collection.extend({
      * unicode Katakana range
      * 30A0 ~ 30FF */
     if (Math.random() < 0.5) {
-      return unescape(
-        '%u30' + randomHex(4, 9) + randomHex(0, 6)
-      );
+      return unescape(`%u30${randomHex(4, 9)}${randomHex(0, 6)}`);
     } else {
-      return unescape(
-        '%u30' + randomHex('A', 'F') + randomHex(0, 'F')
-      );
+      return unescape(`%u30${randomHex('A', 'F')}${randomHex(0, 'F')}`);
     }
   },
   randomKanji: function() {
     /* unicode Kanji range
      * 4E00 ~ 9FC3 */
-    return unescape(
-      '%u' + randomHex(4, 9) + randomHex('E', 'F') +
-             randomHex(0, 'C') + randomHex(0, 3));
+    return unescape(`%u${randomHex(4, 9)}${randomHex('E', 'F')}${randomHex(0, 'C')}${randomHex(0, 3)}`);
   },
   randomHangul: function() {
     /* unicode Hangul range
      * AC00 ~ AFFF */
-    return unescape(
-      '%uA' + randomHex('C', 'F') + randomHex(0, 'F') + randomHex(0, 'F')
-    );
+    return unescape(`%uA${randomHex('C', 'F')}${randomHex(0, 'F')}${randomHex(0, 'F')}`);
   },
   randomSymbol: function() {
     /* unicode Symbol range
      * 2600 ~ 269D */
-    return unescape(
-      '%u26' + randomHex(0, 9) + randomHex(0, 'D')
-    );
+    return unescape(`%u26${randomHex(0, 9)}${randomHex(0, 'D')}`);
   },
   snowMan: function() {
     /* unicode Snowman
