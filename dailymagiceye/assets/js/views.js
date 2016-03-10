22708678afcca94ebc6d7bd7acacedee0decda84
aminami1127 <aminami1127@users.noreply.github.com>
2016-03-10 22:31:11 +0900

    change option of introjs

--- dailymagiceye/assets/js/views.js
+++ dailymagiceye/assets/js/views.js
@@ -135,32 +135,30 @@ export var SideView = Marionette.CompositeView.extend({
     var intro = introJs.introJs();
     var link1 = "https://en.wikipedia.org/wiki/Autostereogramhttps://en.wikipedia.org/wiki/Autostereogram";
     var link2 = "http://www.colorstereo.com/texts_.txt/practice.htm";
+    intro.setOption('showStepNumbers', false);
+    intro.setOption('tooltipPosition', 'auto');
+    intro.setOption('positionPrecedence', ['left', 'right', 'bottom', 'top']);
     intro.setOptions({
       steps: [
         {
           element: document.querySelector('#title'),
           intro: `Welcome to Daily MagicEye! This is a random word <a href=${link1} target="_blank">autostereogram</a> app for visual recovery(I hope).`,
-          position: "bottom"
         },
         {
           element: document.querySelectorAll('#magic-eye')[0],
           intro: `Here's the MagicEye image, one word is floating on it. <a href=${link2} target="_blank">Can you find it?</a>`,
-          position: "top"
         },
         {
           element: '#textList',
           intro: "You can choose the floating word here.",
-          position: "top"
         },
         {
           element: '#nav',
           intro: "You can also select type of words here.",
-          position: "top"
         },
         {
           element: '#refresh',
-          intro: "The words are generated randomly. So you can reload them.",
-          position: "top"
+          intro: "The words are generated randomly. Press this button to reload them.",
         },
       ]
     });
