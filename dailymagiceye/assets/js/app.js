6d1c5e1f31f9f8a59a01543507b44e91818ccbdd
Atsuo Minami <a.minami79@gmail.com>
2016-05-25 08:34:19 +0900

    Update app.js

--- dailymagiceye/assets/js/app.js
+++ dailymagiceye/assets/js/app.js
@@ -3,7 +3,7 @@ import Marionette from 'backbone.marionette';
 import { IMAGE_WIDTH, IMAGE_HEIGHT } from 'assets/js/settings';
 import { Image, TextList } from 'assets/js/models';
 import { ImageView, SideView } from 'assets/js/views';
-import {getImageSize} from 'assets/js/utils';
+import { getImageSize } from 'assets/js/utils';
 
 $(function(){
   var App = new Marionette.Application();
